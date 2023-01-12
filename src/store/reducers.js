ba52361a2a15e7c391acb8d9f5c3a1a360aef1b4
batMuffin12 <ofekbensimhon.work@gmail.com>
2023-01-12 13:36:53 +0200

    added base for store

---
+++ src/store/reducers.js
@@ -0,0 +1,16 @@
+import { models } from "appConstants/models";
+import { composeReducers, makeAsyncReducer, makeReducer } from "redux-toolbelt";
+import { capitalize } from "utils/storeUtils";
+import actions from "./actions";
+
+const dynamicReduces = models.map(({ model, customerMethodName }) => ({
+  [model]: composeReducers(
+    makeAsyncReducer(actions[customerMethodName || `get${capitalize(model)}`], {
+      defaultData: [],
+    })
+  ),
+}));
+
+export default {
+  ...dynamicReduces,
+};
